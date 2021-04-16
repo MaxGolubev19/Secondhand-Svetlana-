281f5d94e9b3eb795b661ebdd3ebabc8e164691b
MaxGolubev19 <75436888+MaxGolubev19@users.noreply.github.com>
2021-04-16 19:11:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -51,14 +51,13 @@ def account():
     params['title'] = 'Аккаунт'
     return render_template('account.html', **params)
 
-"""
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-    
+
 """
-
-
 if __name__ == '__main__':
     db_session.global_init("db/shop.db")
     app.run()
+"""
