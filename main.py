83d34992c45a370eeca96230ae7300b20cd531c5
MaxGolubev19 <75436888+MaxGolubev19@users.noreply.github.com>
2021-04-23 23:10:22 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -30,6 +30,49 @@ categories = {'Одежда': 'wear',
             'Другое': 'other',
     }
 
+sizes = {'2XS': 'size1',
+         'XS': 'size2',
+         'M': 'size3',
+         'L': 'size4',
+         'XL': 'size5',
+         '2XL': 'size6',
+         '3XL': 'size7',
+         '4XL': 'size8',
+         '6.5US': 'size1',
+         '7US': 'size2',
+         '7.5US': 'size3',
+         '8US': 'size4',
+         '8.5US': 'size5',
+         '9US': 'size6',
+         '9.5US': 'size7',
+         '10US': 'size8',
+         '10.5US': 'size9',
+         '11US': 'size10',
+         '11.5US': 'size11',
+         '10см': 'size11',
+         '11см': 'size11',
+         '12см': 'size12',
+         '13см': 'size13',
+         '14см': 'size14',
+         '15см': 'size15',
+         '16см': 'size16',
+         '17см': 'size17',
+         '18см': 'size18',
+         '19см': 'size19',
+         '20см': 'size20',
+         '21см': 'size21',
+         '22см': 'size22',
+         '23см': 'size23',
+         '24см': 'size24',
+         '25см': 'size25',
+         '26см': 'size26',
+         '27см': 'size27',
+         '28см': 'size28',
+         '29см': 'size29',
+         '30см': 'size30',
+    }
+
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -54,6 +97,7 @@ def catalog():
     if request.method == 'POST':
         my_sex = sex.get(request.form.get('sex'), None)
         my_category = categories.get(request.form.get('category'), None)
+        my_size = request.form.get('size')
         if my_sex:
             products = products.filter(Product.sex == my_sex)
             params[f'{my_sex}'] = 'selected="selected"'
@@ -61,7 +105,10 @@ def catalog():
             params['my_category'] = my_category
             products = products.filter(Product.category == my_category)
             params[f'{my_category}'] = 'selected="selected"'
-        params['products'] = products
+        if my_size:
+            products = products.filter(Product.size == my_size)
+            params[f'{sizes[my_size]}'] = 'selected="selected"'
+    params['products'] = products
     return render_template('catalog.html', **params)
 
 
@@ -74,6 +121,7 @@ def category(category):
     if request.method == 'POST':
         my_sex = sex.get(request.form.get('sex'), None)
         my_category = categories.get(request.form.get('category'), None)
+        my_size = request.form.get('size')
         if my_sex:
             products = products.filter(Product.sex == my_sex)
             params[f'{my_sex}'] = 'selected="selected"'
@@ -81,6 +129,9 @@ def category(category):
             params['my_category'] = my_category
             products = products.filter(Product.category == my_category)
             params[f'{my_category}'] = 'selected="selected"'
+        if my_size:
+            products = products.filter(Product.size == my_size)
+            params[f'{sizes[my_size]}'] = 'selected="selected"'
     else:
         products = products.filter(Product.sex == category)
         params[f'{category}'] = 'selected="selected"'
